01b645080b6bb513a5c534894d9d156c6313e39a
Jeremy Evans <jeremy.n.evans@gmail.com>
2016-10-05 11:15:11 +0100

    Webhook

--- index.js
+++ index.js
@@ -19,3 +19,18 @@ app.listen(app.get('port'), function() {
 });
 
 var rssBuilder = require('./rssBuilder.js');
+
+
+
+//Facebook Chatbot
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'explain_the_news') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
